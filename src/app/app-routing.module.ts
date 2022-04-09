2292172935c6fe331802cc41eb473eb915a262a0
Andrew Nguyen <andrewnguyen@andrews-mbp-2.mynetworksettings.com>
2022-04-08 21:22:07 -0400

    created registration and verify-email pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
+  },
+  {
+    path: 'verify-email',
+    loadChildren: () => import('./verify-email/verify-email.module').then( m => m.VerifyEmailPageModule)
   }
 ];
 @NgModule({
