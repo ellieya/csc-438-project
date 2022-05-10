bfe0eafc8033c2e57981194aeb22e857d5245233
Ellie <j.chen.ellie@gmail.com>
2022-05-10 09:47:49 -0400

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,6 +30,12 @@ import { environment } from '../environments/environment';
     AngularFirestoreModule,
     AngularFireStorageModule,
     AngularFireDatabaseModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyANhvVDAvXjiVChJHBHSBAIyl0vIpc43NQ",
+      authDomain: "csc438-project.firebaseapp.com",
+      projectId: "csc438-project",
+      storageBucket: "csc438-project.appspot.com",
+      messagingSenderId: "1010866971079",
+      appId: "1:1010866971079:web:6906ef2cde44ed3f56f9e2",
+      measurementId: "G-E96746GK3M"
+  }
 };
