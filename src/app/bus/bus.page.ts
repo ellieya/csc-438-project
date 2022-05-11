e24d7adce227caede876a033a4169d9cbf2cff3a
Nick Palermo <nicholas.palermo8@gmail.com>
2022-05-11 04:27:09 -0400

    Added Bus Stop Functionality

--- src/app/bus/bus.page.ts
+++ src/app/bus/bus.page.ts
@@ -13,17 +13,30 @@ export class BusPage implements OnInit {
   res = {};
   oba = [];
   busRoutes;
+  shortName;
+  longName;
+  stopName;
+  arrivalTime;
+  stopsAway;
+  description;
 
   ngOnInit() {
-    this.http.get("https://us-central1-csc438-project.cloudfunctions.net/app/bus/stop_data/MTA_300006").subscribe( (res:any) => {
-      this.res = res; 
-      this.oba=res.OBA[0]; 
-    });
-  }
+    this.http.get("https://us-central1-csc438-project.cloudfunctions.net/app/bus/stop_data/MTA_200021").subscribe( (res:any) => {
+      this.res = res;
+      this.oba=res.OBA[0];
 
-  logRes() {
-    console.log(this.res);
-    console.log(this.oba);
+      this.stopName = res.OBA[0].name[0] //Stop intersection
+
+      this.shortName = res.OBA[0].routes[0].route[0].shortName[0];
+
+      this.longName = res.OBA[0].routes[0].route[0].longName[0]
+
+      this.description = res.OBA[0].routes[0].route[0].description[0];
+
+      this.arrivalTime = res.SIRI.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit[0].MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime.substr(11,5)
+      
+      this.stopsAway = res.SIRI.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit[0].MonitoredVehicleJourney.MonitoredCall.NumberOfStopsAway;
+    });
   }
 
 }
